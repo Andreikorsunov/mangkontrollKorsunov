0e3db1714f1baf2dc180ba65d464747a7d93ae57
opilane <opilane@HP-CZC5261P8F>
2022-09-09 10:32:34 +0300

    Add project files.

---
+++ mangkontrollKorsunov/Ese.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mangkontrollKorsunov
+{
+    abstract class Ese : Uksus
+    {
+        private string nimi;
+        private int punktidearv;
+        public Ese(string nimi, int punktidearv)
+        {
+            this.nimi = nimi;
+            this.punktidearv = punktidearv;
+        }
+        public override int punktideArv()
+        {
+            return punktidearv;
+        }
+        public override string Info()
+        {
+            return nimi;
+        }
+    }
+}
---
+++ mangkontrollKorsunov/Tegelane.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mangkontrollKorsunov
+{
+    abstract class Tegelane : Uksus
+    {
+        private string nimi;
+        private List<Ese> Asjad = new List<Ese>() { };
+        public Tegelane(string nimi)
+        {
+            this.nimi = nimi;
+        }
+        public void LisaEse()
+        {
+            
+        }
+        
+    }
+}
